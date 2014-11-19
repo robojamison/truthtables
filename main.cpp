86c7c0cd2f26d70d3a722d9f321d336f12c7b0c0
Jamison Bryant <robojamison@gmail.com>
2014-11-18 21:05:24 -0500

    Created project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Jamison
+ *
+ * Created on November 18, 2014, 8:56 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
