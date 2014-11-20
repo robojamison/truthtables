b44ddde0debd60a9b8c5e34b0df3fd6da46a059c
Jamison Bryant <robojamison@gmail.com>
2014-11-20 01:38:05 -0500

    Added basic CLI argument parsing

--- main.cpp
+++ main.cpp
@@ -1,19 +1,57 @@
-/* 
- * File:   main.cpp
- * Author: Jamison
- *
- * Created on November 18, 2014, 8:56 PM
+/** 
+ * Main file
+ * 
+ * Author:   Jamison Bryant <robojamison@gmail.com>
+ * Filename: main.cpp
+ * Version:  1.0
  */
 
 #include <cstdlib>
+#include <stdio.h>
+#include <string>
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
-
+int main(int argc, char** argv) 
+{
+    bool debug_mode = false;
+    
+    // Check if arguments given
+    if(argc > 0) {
+        // Parse and validate given arguments
+        bool args_valid = false;
+        
+        for(int i = 0; i < argc; i++) {
+            string this_arg = string(argv[i]);
+            
+            // Check if debug mode requested
+            if(this_arg == "-d") {
+                string next_arg = toupper(string(argv[i + 1]));
+                
+                if(next_arg == "Y") {
+                    debug_mode = true;
+                } else if(next_arg == "N") {
+                    debug_mode = false;
+                }
+            } 
+            
+            // Check if input string given
+            if(this_arg == "-i") {
+                string next_arg = string(argv[i + 1]);
+                string input_str = next_arg;
+                args_valid = true;
+            }
+        }
+        
+        if(!args_valid) {
+            printf("ERROR: One or more arguments were invalid or missing.");
+            
+            return 0;
+        }
+    } else {
+        // No arguments given: enter interactive mode
+    }
+    
     return 0;
 }
 
